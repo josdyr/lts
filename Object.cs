bd38c55fd97d8e978b6998156476922425f117cf
Jostein Dyrseth <josdyr@gmail.com>
2023-11-26 22:37:19 +0100

    Add Object class with required properties

---
+++ Object.cs
@@ -0,0 +1,7 @@
+public class Object
+{
+    public required int GUID { get; set; }
+    public required string Model { get; set; }
+    public required string SerialNumber { get; set; }
+    public required string Location { get; set; }
+}
