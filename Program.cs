31bd02fe52da0af2ae00e0d7e48dddcb2e597e16
Jostein Dyrseth <josdyr@gmail.com>
2023-11-26 22:38:46 +0100

    Add Swagger and InMemory DbContext configuration

--- Program.cs
+++ Program.cs
@@ -1,18 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore.SqlServer;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(); // Learn more at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddDbContext<LtsDbContext>(opt => opt.UseInMemoryDatabase("ObjectList"));
+// builder.Services.AddDbContext<TodoDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
