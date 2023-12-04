93e9740b3fa19df03809548d3dea16e798c32e22
Jostein Dyrseth <josdyr@gmail.com>
2023-12-04 12:50:29 +0100

    Add required attribute validation to TeslaCar properties

--- Models/TeslaCar.cs
+++ Models/TeslaCar.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 public class TeslaCar
 {
     public int Id { get; set; }
+    [Required]
     public string? Model { get; set; }
+    [Required]
     public string? SerialNumber { get; set; }
+    [Required]
     public string? Location { get; set; }
 }
