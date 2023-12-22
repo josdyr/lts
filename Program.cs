d53b596e825e5c4246c5c3039292dc95d2a7bc05
Jostein Dyrseth <josdyr@gmail.com>
2023-12-22 14:14:20 +0100

    Revert Back One Step

--- Program.cs
+++ Program.cs
@@ -2,8 +2,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore.SqlServer;
 using Azure.Messaging.WebPubSub;
-using Azure.Identity;
-using Azure.Security.KeyVault.Secrets;
 using lts.Models;
 
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
@@ -24,13 +22,7 @@ builder.Services.AddCors(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(); // Learn more at https://aka.ms/aspnetcore/swashbuckle
 
-var keyVaultUrl = builder.Configuration["AzureKeyVault:VaultUrl"];
-var credential = new DefaultAzureCredential();
-var client = new SecretClient(new Uri(keyVaultUrl), credential);
-var secret = client.GetSecret("ConnectionStringSecretName");
-var connectionString = secret.Value.Value;
-
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
